5a203ebafd0d31f8dce1e7e92a6207331ce589bc
Hardik Sachan <hardik0casr@gmail.com>
2025-07-13 17:17:50 +0530

    created 3 apis for accounts using gin framework

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+
+	"github.com/Hardik5862/simplebank/api"
+	db "github.com/Hardik5862/simplebank/db/sqlc"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+const (
+	dbSource      = "postgresql://postgres:postgres@localhost:5432/simplebank?sslmode=disable"
+	serverAddress = "0.0.0.0:8005"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	conn, err := pgxpool.New(context.Background(), dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
 }
